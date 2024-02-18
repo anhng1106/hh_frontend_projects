d4ddc6a832ea51044a4ac3699e7417013e93520f
Anh Nguyen <123630454+anhng1106@users.noreply.github.com>
2024-02-18 15:09:38 +0200

    Add files via upload

---
+++ TodoList/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ToDoList from "./components/ToDoList";
+
+function App() {
+  return (
+    <>
+      <div class="box">
+        <h2>Simple Todolist</h2>
+      </div>
+      <ToDoList />
+    </>
+  );
+}
+
+export default App;
